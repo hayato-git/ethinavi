f26dcc6bd8c4e66d163a8963bb7f9d10b76500ea
岸颯人 <hayatokishi@MacBook-Pro.local>
2019-06-08 12:09:51 +0900

    Add: controllerを追加する

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'home/about'
+  get 'users/edit'
+  get 'comments/new'
+  get 'genres/index'
+  get 'genres/new'
+  get 'genres/show'
+  get 'genres/edit'
+  get 'shops/index'
+  get 'shops/new'
+  get 'shops/show'
+  get 'shops/edit'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
