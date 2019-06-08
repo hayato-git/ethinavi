fa8ac48d0ccca8578f39e48862e4126fd8754a3e
岸颯人 <hayatokishi@MacBook-Pro.local>
2019-06-08 12:20:18 +0900

    Add: routingを追加する

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/about'
-  get 'users/edit'
-  get 'comments/new'
-  get 'genres/index'
-  get 'genres/new'
-  get 'genres/show'
-  get 'genres/edit'
-  get 'shops/index'
-  get 'shops/new'
-  get 'shops/show'
-  get 'shops/edit'
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root "shops#index"
+    devise_for :users
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :users, only:[:edit,:update]
+    resources :shops do
+      resource :comments, only:[:new,:create,:destroy]
+    end
+    delete "/shops/:id" => "shops#destroy", as: "destroy_shop"
+    get "home/about" => "home#about"
 end
