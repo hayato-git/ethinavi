f99029920332ea9ae093f28c1c1403faf31e2b16
岸颯人 <hayatokishi@MacBook-Pro.local>
2019-06-11 21:17:49 +0900

    Add: お店一覧投稿画面を追加する

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,6 +1,20 @@
 class ShopsController < ApplicationController
 
 	  def index
+      # genre関係なく全て取り出す
+      @shops = Shop.all.includes(:genres)
+      # genreが0(食料・飲料)に当てはまるもの
+      @genre = Genre.where(genre: 0)
+      # genreが1(ファッション)に当てはまるもの
+      @genre_one = Genre.where(genre: 1)
+      # genreが2(日用品)に当てはまるもの
+      @genre_two = Genre.where(genre: 2)
+      # genreが3(雑貨)に当てはまるもの
+      @genre_three = Genre.where(genre: 3)
+      # genreが4(地産地消)に当てはまるもの
+      @genre_four = Genre.where(genre: 4)
+      # genreが5(セレクトショップ)に当てはまるもの
+      @genre_five = Genre.where(genre: 5)
     end
 
   	def new
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7eaa8f7a7c672338f5604a708dbe8a311ec863d16b7ffbe4419e697f57eb9b66349cc38d5808da3ef344c1ddc73bcb7d93e5e60254d4476ad374370e7fc0c37f'
