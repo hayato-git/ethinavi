cc03628c469a480fd75c3b90356a7246e62b1dfd
岸颯人 <hayatokishi@MacBook-Pro.local>
2019-06-22 10:14:21 +0900

    Add: users_controllerにdef correct_userを追加する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,30 @@
 class UsersController < ApplicationController
 
-	def edit
-		@user = User.find(params[:id])
-  	end
+  before_action :correct_user, only: [:edit, :update]
 
-  	def update
-  		@user = User.find(params[:id])
-  		if @user.update(user_params)
-  		    redirect_to("/shops")
-  		else
-  			render("users/edit")
-  		end
-  	end
+	  def edit
+		  # @user = User.find(params[:id])
+    end
 
-  	private
+    def update
+  	  # @user = User.find(params[:id])
+  	  if @user.update(user_params)
+  	    redirect_to("/shops")
+  	  else
+  	    render("users/edit")
+  	  end
+    end
+
+  private
 
   	def user_params
   		params.require(:user).permit(:userimage, :name, :email)
   	end
+
+    # 正しいユーザーどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to("/shops") unless @user == current_user
+    end
 
 end
