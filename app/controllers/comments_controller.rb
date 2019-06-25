b9a738dac1941741e039845642af42f0b7c58268
岸颯人 <hayatokishi@MacBook-Pro.local>
2019-06-26 00:06:22 +0900

    Add: コメント削除機能を追加する

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,19 @@
 class CommentsController < ApplicationController
+
   	def create
-      shop = Shop.find(params[:shop_id])
-      comment = current_user.comments.new(comment_params)
-      comment.shop_id = shop.id
-      comment.save
-      redirect_to shop_path(shop)
+      @shop = Shop.find(params[:shop_id])
+      @comment = @shop.comments.build(comment_params)
+      @comment.user_id = current_user.id
+      @comment.save
+      redirect_to shop_path(@shop)
   	end
+
+    def destroy
+      @shop = Shop.find(params[:shop_id])
+      @comment = @shop.comments.find(params[:id])
+      @comment.destroy
+      redirect_to shop_path(@shop)
+    end
 
   	private
 
